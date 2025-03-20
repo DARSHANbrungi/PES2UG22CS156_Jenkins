65fb7861672e2710ef468fc2f1586e36ed00320c
DARSHAN B M <143056811+DARSHANbrungi@users.noreply.github.com>
2025-03-20 10:13:21 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
-  cout << "I made some changes\n";
+  cout << "I made two changes\n";
   return 0;
 }
